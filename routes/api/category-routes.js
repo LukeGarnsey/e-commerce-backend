060b94b62d116f6c76a98f86fa53e5a13c6220e6
Luke Garnsey <lukegarnsey@gmail.com>
2024-02-05 16:49:58 -0500

    setup category and product models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.send("find all catagories");
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.send("find catagories by id");
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  res.send("create new catagory")
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  res.send("update a category");
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  res.send("delete ID");
 });
 
 module.exports = router;
